92c101367708c751fa0aea210af5a3433f931610
Jackster <jackster@163.com>
2021-08-14 21:37:37 +0800

    加入进程控制代码

---
+++ 编程提高/Linux环境编程/环境编程基础/进程控制/execl.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    pid_t pid = fork();
+    if(pid == -1)
+    {
+        perror("fork error");
+        exit(1);
+    }
+    if(pid >0)
+    {
+        sleep(1);
+    }
+    //　子进程执行程序
+    if(pid == 0)
+    {
+        execl("/bin/ls", "ls", "-l", ".", NULL);
+		//char* args[] = {"ls", "-l", ".", NULL};
+		//execv("/bin/ls", args);
+        perror("exec");
+        exit(1);
+    }
+    return 0;
+}
