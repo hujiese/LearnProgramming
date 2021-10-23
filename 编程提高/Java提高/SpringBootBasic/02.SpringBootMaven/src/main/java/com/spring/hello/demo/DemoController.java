8124d2f31009d7d69a44ba575cfdcd4d5ca9c71a
Jackster <jackster@163.com>
2021-10-23 16:00:22 +0800

    使用maven创建spring boot应用

---
+++ 编程提高/Java提高/SpringBootBasic/02.SpringBootMaven/src/main/java/com/spring/hello/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.spring.hello.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot !";
+    }
+}
+
