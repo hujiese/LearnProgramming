1448376bb58d1ffeeed019bc63471e02e9b449b6
Jackster <jackster@163.com>
2021-10-30 20:42:16 +0800

    加入redis操作

---
+++ 编程提高/Java提高/SpringBoot整合篇/01.Redis/src/main/java/com/demo/redis/RedisApplication.java
@@ -0,0 +1,13 @@
+package com.demo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+}
